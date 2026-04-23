3b27c62ca13d068882b3a27954095a153ef95df6
hjpark <86184046+hyojung-Park@users.noreply.github.com>
2026-04-23 09:10:40 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.15 ms, Memory: 9 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -17,4 +17,19 @@ def solution(array):
         
     return ans
 
+'''
+count = {}
+
+for x in array:
+    count[x] = count.get(x, 0) + 1
+    
+max_count = max(count.values())
+
+if list(count.values()).count(max_count) >= 2:
+    retrun -1
+    
+for key, value in count.items():
+    if value == max_count:
+        return key
+'''
     
