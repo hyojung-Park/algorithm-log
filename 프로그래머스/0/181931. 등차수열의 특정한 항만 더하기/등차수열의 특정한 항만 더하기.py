105b5e313ba50d3d9adc04968b42c092089fd6b8
hjpark <86184046+hyojung-Park@users.noreply.github.com>
2026-04-27 10:29:59 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.01 ms, Memory: 9.06 MB -BaekjoonHub

--- 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.py
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.py
@@ -1,13 +1,10 @@
 def solution(a, d, included):
-    ls = [a]
-    for i in range(1, len(included)):
-        ls.append(ls[i-1] + d)
-    
     ans = 0
+    
     for i in range(len(included)):
         if included[i]:
-            ans += ls[i]
-    
+            ans += a + (d * i)
+        
     return ans
     
     
