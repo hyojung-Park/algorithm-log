c0ef4ca1e69df0d7e4dbf78861061ab00b405b1d
hjpark <86184046+hyojung-Park@users.noreply.github.com>
2026-04-28 16:05:53 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 107.97 ms, Memory: 27.1 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,10 +1,19 @@
 def solution(arr):
     ans = [arr[0]] # 첫 인덱스는 그냥 넣어둠
-    idx = 1 # 기준인덱스 지정
-    
-    for i in range(idx, len(arr)):
+
+    for i in range(1, len(arr)):
         if arr[i] != arr[i-1]:
             ans.append(arr[i])
-            idx = i + 1
         
     return ans
+
+'''
+    stack = []
+    
+    for num in arr:
+        if not stack or stack[-1] != num:
+            stack.append(num)
+    
+    return stack
+
+'''
