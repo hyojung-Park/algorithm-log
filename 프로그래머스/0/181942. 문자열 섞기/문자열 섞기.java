e09da6c869a12eb71ce97eefaf67f443dea116dc
hjpark <86184046+hyojung-Park@users.noreply.github.com>
2026-05-01 17:09:28 +0900

    [level 0] Title: 문자열 섞기, Time: 0.08 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -1,14 +1,12 @@
 class Solution {
     public String solution(String str1, String str2) {
-        String answer = "";
-        char [] arr1 = str1.toCharArray();
-        char [] arr2 = str2.toCharArray();
+        StringBuilder sb = new StringBuilder();
         
         for(int i = 0; i < str1.length(); i++){
-            answer += arr1[i];
-            answer += arr2[i];
+            sb.append(str1.charAt(i));
+            sb.append(str2.charAt(i));
         }
         
-        return answer;
+        return sb.toString();
     }
 }
