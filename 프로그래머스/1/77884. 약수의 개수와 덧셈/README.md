d8250ca1df158031bd0bf41387332827668579ca
hjpark <86184046+hyojung-Park@users.noreply.github.com>
2026-05-02 11:57:35 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 45.48 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/README.md
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/README.md
@@ -1,10 +1,10 @@
 # [level 1] 약수의 개수와 덧셈 - 77884 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77884?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77884?language=python3) 
 
 ### 성능 요약
 
-메모리: 78.1 MB, 시간: 4.86 ms
+메모리: 9.19 MB, 시간: 45.48 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 02일 11:53:42
+2026년 05월 02일 11:57:33
 
 ### 문제 설명
 
--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -1,21 +1,14 @@
 def solution(left, right):
-    arr = [i for i in range(left, right+1)]
-    ls = []
     
-    # 1. 약수의 개수 구하기
-    for num in range(left, right+1):
-        cnt = 0
-        for i in range(1, num+1):
-            if num % i == 0:
-                cnt += 1
-        ls.append(cnt)
-    
-    # 2. 짝수라면 더하고 홀수라면 뺴기
     ans = 0
-    for i in range(len(ls)):
-        if ls[i] % 2 == 0:
-            ans += arr[i]
+    for i in range(left, right+1):
+        cnt = 0
+        for j in range(1, right+1):
+            if i % j == 0:
+                cnt += 1
+        if cnt % 2 == 0:
+            ans += i
         else:
-            ans -= arr[i]
+            ans -= i
     
     return ans
