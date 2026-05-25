8171890d8b8c618cea9c0cb7f19a6fec5fd9f8bf
hjpark <86184046+hyojung-Park@users.noreply.github.com>
2026-05-25 15:04:45 +0900

    [level 2] Title: 숫자의 표현, Time: 4.46 ms, Memory: 9.05 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -0,0 +1,17 @@
+def solution(n):
+    answer = 1 # 본인 포함 
+    
+    # 1~ n//2까지 돌리면서 , 내부에서 다시 다음 수 ~ n//2 + 1까지 돌림
+    for i in range(1, n//2+1):
+        s_num = i
+        for j in range(i+1, n//2+2):
+            s_num += j
+            
+            if s_num == n:
+                answer += 1
+                break
+                
+            if s_num > n:
+                break
+    
+    return answer
