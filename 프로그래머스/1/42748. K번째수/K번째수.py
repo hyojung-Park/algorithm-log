7bdd7a16709e5c6e61d83b57a6c8bb2815bf8afa
hjpark <86184046+hyojung-Park@users.noreply.github.com>
2026-06-01 20:18:53 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -7,3 +7,10 @@ def solution(array, commands):
         answer.append(num)
         
     return answer
+
+'''
+    return [
+        sorted(array[i-1:j])[k-1]
+        for i, j, k in commands
+    ]
+'''
