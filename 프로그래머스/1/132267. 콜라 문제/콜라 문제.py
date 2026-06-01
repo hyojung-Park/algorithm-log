3d77a8b1100dd94c5e3b28ba84c2b7aa29263983
hjpark <86184046+hyojung-Park@users.noreply.github.com>
2026-06-01 20:24:34 +0900

    [level 1] Title: 콜라 문제, Time: 0.36 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
@@ -1,15 +1,11 @@
 def solution(a, b, n):
     answer = 0
     while a <= n:
-        # 가져다줄 개수
-        tmp = n // a
-        take = tmp * a
-        # 가져다주고 남은 병의 수
-        n = n - take
-        # 새로 받아오는 병의 수 
-        new = tmp * b
-        # 받아 오고 남은 병의 수
-        n += new
-        answer += new
+        # 교환 횟수
+        exchanged = n // a
+        # 누적 콜라 수 
+        answer += exchanged * b
+        # 다음 턴의 빈 병 수
+        n = n % a + exchanged * b
     
     return answer
