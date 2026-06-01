9fbfd8b1dda0b8f10e7801826151dcca62479bb9
hjpark <86184046+hyojung-Park@users.noreply.github.com>
2026-06-01 15:31:41 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,12 +1,11 @@
 def solution(brown, yellow):
     
-    if yellow == 1:
-        return [3, 3]
-    
-    for i in range(1, yellow):
-        j = yellow / i
-        if int(j) == j:
-            a = i + 2
-            b = j + 2
-            if 2 * a + 2 * b - 4 == brown:
-                return [b, a]
+    for h in range(1, yellow + 1):
+        if yellow % h == 0:
+            w = yellow // h
+
+            carpet_w = w + 2
+            carpet_h = h + 2
+
+            if 2 * carpet_w + 2 * carpet_h - 4 == brown:
+                return [carpet_w, carpet_h]
