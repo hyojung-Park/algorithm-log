e6e1cc0038e9d4002f63338bb1953a215318f162
hjpark <86184046+hyojung-Park@users.noreply.github.com>
2026-06-01 16:30:09 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.01 ms, Memory: 11.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -0,0 +1,36 @@
+def solution(s):
+    answer = ''
+    alpha = {'zero' : '0',
+             'one' : '1',
+             'two' : '2',
+             'three' : '3',
+             'four' : '4',
+             'five' : '5',
+             'six' : '6',
+             'seven' : '7',
+             'eight' : '8',
+             'nine' : '9'
+            }
+           
+    idx = 0
+    while idx < len(s):
+        if s[idx].isdigit():
+            answer += str(s[idx])
+            idx += 1
+            continue
+        else:
+            a = ''
+            while idx < len(s) and not s[idx].isdigit():
+                a += s[idx]
+                if a in alpha:
+                    answer += alpha[a]
+                    idx += 1
+                    break
+                idx += 1
+
+    '''
+    for word, num in alpha.items():
+        s = s.replace(word, num)
+    '''
+    
+    return int(answer)
