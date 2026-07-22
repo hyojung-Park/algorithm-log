3026b1d9f5cc9c2adaa59d7001313eeed7aa560a
hjpark <86184046+hyojung-Park@users.noreply.github.com>
2026-07-22 10:06:06 +0900

    [level 2] Title: 프로세스, Time: 0.25 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+def solution(priorities, location):
+    answer = 0
+    queue = []
+    
+    for i in range(len(priorities)):
+        queue.append([i, priorities[i]])
+    
+    q = deque(queue)
+    
+    while q:
+        now = q.popleft()
+        
+        flag = -1
+        for l, p in q:
+            if p > now[1]:
+                q.append(now)
+                flag = 1
+                break
+        
+        if flag == 1:
+            continue
+        
+        else:
+            answer += 1
+            
+        if now[0] == location:
+            break
+            
+    return answer
