18843711df3797b9cdcfe38efdd21a93da6b86e9
hjpark <86184046+hyojung-Park@users.noreply.github.com>
2026-07-27 08:19:24 +0900

    [level 2] Title: 가장 큰 수, Time: 177.32 ms, Memory: 25.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -0,0 +1,36 @@
+from functools import cmp_to_key
+
+def solution(numbers):
+    answer = ''
+    
+    # 버블정렬 터짐
+#     for i in range(len(numbers) - 1):
+#         for j in range(len(numbers) - 1 - i):
+#             a = str(numbers[j])
+#             b = str(numbers[j+1])
+            
+#             # 두 숫자를 앞뒤 바꿔 붙여서 더 큰 숫자가 앞으로 오도록 정렬
+#             if a + b < b + a:
+#                 numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
+    
+#     for n in numbers:
+#         answer += str(n)
+    
+    numbers = list(map(str, numbers))
+    
+    def compare(a, b):
+        if a + b > b + a:
+            return -1 # a가 b보다 앞
+        elif a + b < b + a:
+            return 1 # b가 a보다 앞 
+        else:
+            return 0 # a, b를 동등하게 취급
+    
+    numbers.sort(key=cmp_to_key(compare))
+    
+    answer = ''.join(numbers)
+    
+    if answer[0] == '0':
+        return '0'
+    
+    return answer
