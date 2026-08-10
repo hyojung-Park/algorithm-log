1b782483e9102ba344de6913fb70b67732e937c1
hjpark <86184046+hyojung-Park@users.noreply.github.com>
2026-08-10 18:21:57 +0900

    [level 2] Title: 타겟 넘버, Time: 110.07 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -0,0 +1,12 @@
+def dfs(numbers, target, idx, total):
+    if idx == len(numbers): # 모든 숫자를 다 처리했을때 
+        if total == target: # target을 만들었다면
+            return 1 # 개수 추가
+        else:
+            return 0 
+    # 빼는 방법과 더하는 방법의 수를 합침
+    return dfs(numbers, target, idx+1, total - numbers[idx]) + dfs(numbers, target, idx+1, total + numbers[idx])
+    
+
+def solution(numbers, target):
+    return dfs(numbers, target, 0, 0)
